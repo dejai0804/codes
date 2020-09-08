3cc2d13274b4d3141d79ff0df4ebe1dc5d1aa9fa
aakinlofa <ayodejiakinlofa@yahoo.com>
2020-09-08 00:35:14 -0400

    WIP

--- ansible/roles/backup/files/pubkey.sh
+++ ansible/roles/backup/files/pubkey.sh
@@ -1,1 +1,4 @@
-cat ~/.ssh/id_rsa.pub | sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no root@10.0.0.237 'cat - >> /root/.ssh/authorized_keys'
+
+PASSWORD=password
+
+cat ~/.ssh/id_rsa.pub | sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no root@10.0.0.237 'cat - >> /root/.ssh/authorized_keys'
